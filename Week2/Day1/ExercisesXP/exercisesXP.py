100a095d3623d11a874291c1c0e7667520432649
KyleKiske <d44i7ryr@yandex.ru>
2023-07-16 14:34:49 +0300

    Added exercisesXp for W2D1

---
+++ Week2/Day1/ExercisesXP/exercisesXP.py
@@ -0,0 +1,63 @@
+# Exercise 1
+
+print("Hello world\n"*4)
+
+# Exercise 2
+
+# print(pow(99,3)*8)
+# print()
+
+# Exercise 3 
+
+# print((5 < 3) == False) 
+# print((3 == 3) == True)
+# print((3 == "3") == False) # it won't throw an error since we can put "==" between str and int, but it will always be false.
+# # print("3" > 3) # will throw an error since We try to compare string and int 
+# print(("Hello" == "hello") == False)
+
+# Exercise  4 
+
+# computer_brand = "lenovo"
+# print(f"I have a {computer_brand} laptop")
+
+# Exercise 5
+
+# name = "Dmitri"
+# age = 26
+# shoe_size = 40
+# info = f"I am {name}. I was born {age} years ago. My shoe size is {shoe_size}. I was professional WoT player. \n"
+# print(info)
+
+# Exercise 6
+
+# a = 2
+# b = 1
+# if (a > b) :
+#     print("Hello World\n")
+
+# Exercise 7
+
+# user_number = int(input("please type any number > "))
+# if (user_number % 2 == 0):
+#     print("the number is even\n")
+# else :
+#     print("the number is odd.\n")
+
+# Exercise 8
+
+# default_name = "Dmitri"
+# user_name = input("Tell us your first name\n").capitalize()
+# if default_name == user_name :
+#     print("Dmitri together strong!\n")
+# else :
+#     print("Better luck next time.\n")
+
+# Exercise 9
+
+
+# height_inches = int(input("Tell us your height in inches > "))
+# print(height_inches*2.54)
+# if (height_inches * 2.54 < 145):
+#     print("Unfortunately you need to grow some more to ride.")
+# else :
+#     print("You're tall enough to ride. Let's GO!")
