29c8117f778a21425f8d4807b41bbfa6f070dc86
KyleKiske <d44i7ryr@yandex.ru>
2023-07-16 14:51:10 +0300

    Added Gold Exercises

---
+++ Week2/Day1/ExercisesXPGold/exercisesXPGold.py
@@ -0,0 +1,17 @@
+#Exercise 1
+
+print("Hello world\n"*4 + "I love python\n"*4)
+
+#Exercise 2
+
+# month = int(input("Please provide a number corresponding to the current month > "))
+# if (month <= 0 or month > 12) :
+#     print("This number doesn't correspond to a month. Did you write the correct value?")
+# elif (month <= 2 or month == 12) :
+#     print("The season is Winter, brr.")
+# elif (month <= 5) :
+#     print("The season is Spring.")
+# elif (month <= 8) :
+#     print("The season is Summer.")
+# else :
+#     print("The season is Autumn.")
