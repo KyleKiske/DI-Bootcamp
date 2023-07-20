610fd233c4fbf3fcdee7a783d8bc2646378506a6
KyleKiske <d44i7ryr@yandex.ru>
2023-07-20 17:05:13 +0300

    Added challenge w2d5

---
+++ Week2/Day5/challenge.py
@@ -0,0 +1,17 @@
+#Challenge 1 : Sorting
+
+print([i for i in sorted(input("wri ").split(','))])
+
+#Challenge 2 : Longest Word
+
+# string = input("Give me words \n")
+# word_list = string.split(' ')
+
+# longest = 0
+# longest_word = ""
+# for w in word_list:
+#     if (w.__len__() > longest):
+#         longest = w.__len__()
+#         longest_word = w
+
+# print(f"longest_word ({string}) -> \"{longest_word}\"")
