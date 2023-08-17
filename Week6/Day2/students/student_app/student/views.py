b0f0ced02832a4cb81f826a062d89b2585ad908d
KyleKiske <d44i7ryr@yandex.ru>
2023-08-17 11:28:18 +0300

    Added W6D2 dc

--- Week6/Day2/students/student_app/student/views.py
+++ Week6/Day2/students/student_app/student/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Student
 from .serializers import StudentSerializer
+from django.utils import timezone
 
 # Create your views here.
 class StudentList(generics.ListCreateAPIView):
-    queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+    def get_queryset(self):
+        queryset = Student.objects.all()
+        date_joined_par = self.request.query_params.get('date_joined')
+        if date_joined_par:
+            try:
+                date_joined_full = timezone.datetime.strptime(date_joined_par, "%Y-%m-%d")
+                print(date_joined_full)
+                queryset = queryset.filter(date_joined__date = date_joined_full.date())
+            except ValueError:
+                pass
+
+        return queryset
+    
 
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
